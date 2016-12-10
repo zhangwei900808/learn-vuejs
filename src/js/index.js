65e6715da99ef77279ff4f7745b8dcb906c031c7
zhangwei900808 <zw900808@gmail.com>
2016-12-10 22:11:15 +0800

    add vue and webpack files

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import Vue from "vue";
+
+let zw = new Vue({
+  el:"#root",
+  data:{
+    name:"zhangwei"
+  }
+})
+
+new Vue(zw);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var htmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+  entry:{
+    "index":__dirname + "/src/js/index.js"
+  },
+  output:{
+    path:__dirname+"/src/webapp/js",
+    filename:'[name].js'
+  },
+  resolve:{
+    alias:{
+      vue:"vue/dist/vue.js"
+    }
+  },
+  externals:{
+
+  },
+  module:{
+    loaders:[
+      {
+        test:/\.js$/,
+        loader:'babel',
+        query:{
+          compact:true
+        }
+      }
+    ]
+  },
+  plugins:[
+    new htmlWebpackPlugin({
+      filename:__dirname+'/src/webapp/index.html',
+      template:__dirname+'/src/html/index.html',
+      inject:'body',
+      hash:true,
+      chunnks:['index']
+    })
+  ]
+}
