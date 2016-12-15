4159bbd9df772e4f1c37b7167c0fa41ce40c72ae
zhangwei900808 <zw900808@gmail.com>
2016-12-15 22:03:38 +0800

    parent componetnt and children componetnts contact

--- src/components/login/user-login.vue
+++ src/components/login/user-login.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="row">
     <form class="form-horizontal" role="form">
-      <user-name ref="uname"  placeholder="请输入用户名"></user-name>
+      <user-name ref="uname"
+        v-on:updateUsername="setUsername" placeholder="请输入用户名"></user-name>
       <user-submit></user-submit>
+      <a href="#" class="btn btn-primary" v-on:click="show">测试</a>
     </form>
   </div>
 </template>
@@ -13,6 +15,19 @@ import username from "./user/user-name.vue";
 import usersubmit from "./user/user-submit.vue";
 
 export default{
+  data(){
+    return {
+      username:""
+    }
+  },
+  methods:{
+    setUsername(uname){
+      this.username = uname;
+    },
+    show(){
+      alert(this.username);
+    }
+  },
   components:{
     "user-name":username,
     "user-submit":usersubmit
--- src/components/login/user/user-name.vue
+++ src/components/login/user/user-name.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="form-group">
     <label>用户名</label>
-    <input type="text" v-model="username" class="form-control" :placeholder="placeholder">
+    <input v-on:change="userNameChange" type="text" v-model="username" class="form-control" :placeholder="placeholder">
   </div>
 </template>
 
 <script>
   export default {
     props:["placeholder"],
+    methods:{
+      userNameChange(){
+        this.$emit("updateUsername",this.username)
+      }
+    },
     data:function(){
       return {
         username:""
