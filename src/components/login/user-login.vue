7c1f6f13c2fbd95fef499b5ba3e6c13cf3e2e762
zhangwei900808 <zw900808@gmail.com>
2016-12-21 21:59:27 +0800

    add computed property and prev add mounted property

--- src/components/login/user-login.vue
+++ src/components/login/user-login.vue
@@ -5,6 +5,7 @@
       <h2 v-else>用户登录</h2>
       <user-name ref="uname" placeholder="请输入用户名"></user-name>
       <user-area></user-area>
+      <user-favs></user-favs>
       <user-submit></user-submit>
       <a href="#" class="btn btn-primary" v-on:click="show">测试</a>
     </form>
@@ -16,6 +17,7 @@
 import username from "./user/user-name.vue";
 import usersubmit from "./user/user-submit.vue";
 import userarea from "./user/user-area.vue";
+import userfavs from "./user/user-favs.vue";
 
 export default{
   mounted(){
@@ -41,7 +43,8 @@ export default{
   components:{
     "user-name":username,
     "user-submit":usersubmit,
-    "user-area":userarea
+    "user-area":userarea,
+    "user-favs":userfavs
   }
 }
 
---
+++ src/components/login/user/user-favs.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="form-group">
+    <label>你的爱好：</label>
+    <div>
+      <input type="text" class="form-control" v-model="inputTxt">
+      <table v-if="showTable()">
+        <thead>
+          <th>类别1</th>
+          <th>类别2</th>
+        </thead>
+        <tbody>
+          <tr v-for="fav in getFavs">
+            <td>{{fav.class1}}</td>
+            <td>{{fav.class2}}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return {
+        inputTxt:'',
+        favs:[
+          {
+            'class1':'开发类',
+            'class2':'js'
+          },
+          {
+            'class1':'编程类',
+            'class2':'java'
+          }
+        ]
+      }
+    },
+    methods:{
+      showTable(){
+        return this.inputTxt != "";
+      }
+    },
+    computed:{
+      getFavs(){
+        return this.favs.filter(function(value){
+          return value.class2.indexOf(this.inputTxt) >=0 ;
+        }.bind(this));
+      }
+    }
+  }
+</script>
