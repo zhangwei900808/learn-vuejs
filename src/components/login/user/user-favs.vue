bd0df0b03a7b4b6ca4c4feb768109212ca518603
zhangwei900808 <zw900808@gmail.com>
2016-12-22 21:35:34 +0800

    update

--- src/components/login/user/user-favs.vue
+++ src/components/login/user/user-favs.vue
@@ -11,7 +11,9 @@
         <tbody>
           <tr v-for="fav in getFavs">
             <td>{{fav.class1}}</td>
-            <td>{{fav.class2}}</td>
+            <td><a href="#" v-for="code in filterClass2(fav.class2)">
+              {{code}}
+            </a></td>
           </tr>
         </tbody>
       </table>
@@ -27,11 +29,19 @@
         favs:[
           {
             'class1':'开发类',
-            'class2':'js'
+            'class2':[
+              'js',
+              'html',
+              'css'
+            ]
           },
           {
             'class1':'编程类',
-            'class2':'java'
+            'class2':[
+              'java',
+              'php',
+              'python'
+            ]
           }
         ]
       }
@@ -39,12 +49,22 @@
     methods:{
       showTable(){
         return this.inputTxt != "";
+      },
+      filterClass2(val){
+        return val.filter(function(v){
+          return v.indexOf(this.inputTxt)>=0;
+        }.bind(this));
       }
     },
     computed:{
       getFavs(){
         return this.favs.filter(function(value){
-          return value.class2.indexOf(this.inputTxt) >=0 ;
+          if(value.class2.filter(function(v){
+            return v.indexOf(this.inputTxt)>=0;
+          }.bind(this)).length>0){
+            return true;
+          }
+          return false ;
         }.bind(this));
       }
     }
